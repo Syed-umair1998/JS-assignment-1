d3d1df13930273e7dfe3a6ca471793b048e01384
Syed-umair1998 <160543227+Syed-umair1998@users.noreply.github.com>
2024-03-09 04:10:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,130 @@
+// CHAPTER # 1
+
+// question #2
+// alert("Error! please enter a valid password.")
+
+// question #3
+// alert("welcome to Js Land... \nHappy Coding!")
+
+// question #4
+// alert("welcome to Js Land...")
+
+// alert("Happy Coding!  \nprevent this page from creating additional dialogs.")
+
+// question #5
+// alert("Hello... i can run JS through my web browser,s console")
+
+// question #7
+// solution (c) body insight your pages HTml
+
+// CHAPTER # 2
+
+// question #1
+// let username= "umair"
+// console.log(username);
+
+// question #2
+// let myname= "Umair " 
+// let fullname= "Abdul Sattar"
+// let merge=myname+fullname
+// console.log(merge);
+
+// question #3
+// const message= "Hello World"
+// alert("hello world")
+
+// question #4
+// alert("jhone doe")
+// alert("15 years old")
+// alert("certified mobile app development")
+
+// question #5
+// var food= "pizza \npizz \npiz \npi \np";
+// alert(food);
+
+// question #6
+// var email="my email address is umair.waqas.uw@gmail.com";
+// alert(email);
+
+// question #7
+// var book="A smarter way to learn javascript";
+// alert(book);
+
+// question #8
+// document.write("<h3>Yah! i can write html content through javascript</h3>")
+
+// question #9
+// var string="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(string);
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// CHAPTER #3
+
+// question #1
+// var age="I am 25 years old";
+// alert(age);
+
+// question #2
+// var visits="you have visited this site 14 times";
+// alert(visits);
+
+// question #3
+// var birthyear="1998"
+// alert("my birth year is" + birthyear);
+// document.write("My birth year is 1998 <br>data types of my declared variable is number");
+
+// question #4
+// var xyzclothing=("afzal, shirts, 5T-shirts")
+// document.write("jhone Doe ordered 5 T-shirt(s) on XYZ Clothing store.")
+// console.log(xyzclothing);
+
+// CHAPTER #4
+
+// question #1
+// (1) let
+// (2) const
+// (3) var
+
+// question #2
+//5 legal variables
+// (1) umair
+// (2)_umair
+// (3)umairWaqas
+// (4)$umair
+// (5)myUmair123
+
+//5 illegal variables
+// (1) 2umair
+// (2)_umair-waqas
+// (3)umair Waqas
+// (4)umair@waqas
+// (5)reserved keyords like var const and let is illegal
+
+// question #3
+// (b) $umair, _umair, Umair, umair_waqas
+// (c) characters, $, _
+// (d) caseSensitive
+// (e) keywords
+
+// CHAPTER #5
+
+// question #1
+// var num1=5;
+// var num2=3;
+// var sum=num1+num2
+// console.log(sum);
+// document.write("sum of 5 + 3 is 8")
+
+
+// concentation
+// var num1="100";
+// var num2="200";
+// var add=num1+num2
+// console.log(add);
+
+// // DMAS
+// var num4=5;
+// var num5=10;
+// var num6=2;
+// var result = num4 + num5 * num5 / num6
+// console.log(result);
